a02d4da8a0688acd7f10c8d5f3f7491cfc248e66
Nicolae Florin Munteanu <1899198@crosemont.qc.ca>
2021-10-15 16:44:05 -0400

    Update la page Random

--- src/pages/PageRandom.jsx
+++ src/pages/PageRandom.jsx
@@ -1,20 +1,44 @@
-import React, { useState, useEffect }  from 'react'
+import React, { useState } from 'react'
+import { Card } from 'semantic-ui-react';
 
 const PageRandom = () => {
-
-    const [randomQuote, setRandomQuote] = useState();
+    const [randomQuote, setRandomQuote] = useState({});
     const [erreur, setErreur] = useState('');
 
-    useEffect(() => {
-        fetch('https://jservice.io/api/random')
-            .then((response) => JSON.stringify(response).json())
-            .then((data) => setRandomQuote(data))
-            .catch((er) => setErreur(er));
-        console.log(randomQuote);
-    }, []);
+    const afficherQuestion = async () => {
+        try {
+            let response = await fetch('https://jservice.io/api/random');
+            let data = await response.json()
+            setRandomQuote(data[0]);
+        }
+        catch (e) {
+            setErreur("Erreur lors de l'appel à l'API");
+        }
+    };
 
     return (
-        <h1>Simple API question trivia aléatoire</h1>
+        <>
+            <h1>Simple API question trivia aléatoire</h1>
+            <button onClick={afficherQuestion}>Question Aléatoire</button>
+            <Card className="centered">
+                <Card.Content>
+                    <Card.Header>Id</Card.Header>
+                    <Card.Description>{randomQuote.id}</Card.Description>
+                </Card.Content>
+                <Card.Content>
+                    <Card.Header>Question</Card.Header>
+                    <Card.Description>{randomQuote.question}</Card.Description>
+                </Card.Content>
+                <Card.Content>
+                    <Card.Description>Date{randomQuote.airdate}</Card.Description>
+                </Card.Content>
+                <Card.Content>
+                    <Card.Header>Réponse</Card.Header>
+                    <Card.Description>{randomQuote.answer}</Card.Description>
+                </Card.Content>
+            </Card>
+            <h2>{erreur}</h2>
+        </>
     )
 }
 
